3c7443aeb79aa818e7417fd1a319166fe16ec071
xarly1602 <xarly1602@gmail.com>
2014-12-05 17:00:35 -0300

    Primer commit

---
+++ src/com/tutecentral/navigationdrawer/MainActivity.java
@@ -0,0 +1,278 @@
+package com.tutecentral.navigationdrawer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+	CustomDrawerAdapter adapter;
+
+	List<DrawerItem> dataList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Initializing
+		dataList = new ArrayList<DrawerItem>();
+		mTitle = mDrawerTitle = getTitle();
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
+
+		// Add Drawer Item to dataList
+		dataList.add(new DrawerItem(true)); // adding a spinner to the list
+
+		dataList.add(new DrawerItem("Buscar...")); // adding a header to the list
+		dataList.add(new DrawerItem("Restaurante", R.drawable.ic_action_restaurant));
+		dataList.add(new DrawerItem("Hoteles", R.drawable.ic_action_hotel));
+		dataList.add(new DrawerItem("Agencias", R.drawable.ic_action_agencia));
+		dataList.add(new DrawerItem("Lugares Turísticos", R.drawable.ic_action_lugarturistico));
+                dataList.add(new DrawerItem("Realidad Aumentada", R.drawable.ic_action_ar));
+
+		dataList.add(new DrawerItem("Utilidades"));// adding a header to the list
+		dataList.add(new DrawerItem("Catálogo de agencias", R.drawable.ic_action_catalogo));
+		dataList.add(new DrawerItem("Conversor", R.drawable.ic_action_dinero));
+		dataList.add(new DrawerItem("Teléfonos de emergencia", R.drawable.ic_action_telefono));
+		dataList.add(new DrawerItem("Frases", R.drawable.ic_action_frases));
+
+		 dataList.add(new DrawerItem("Otros")); // adding a header to the list
+		dataList.add(new DrawerItem("Acerca de TripTips", R.drawable.ic_action_info));
+		dataList.add(new DrawerItem("Configuraciones", R.drawable.ic_action_config));
+		dataList.add(new DrawerItem("Ayuda", R.drawable.ic_action_ayuda));
+
+		adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item,
+				dataList);
+
+		mDrawerList.setAdapter(adapter);
+
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		if (savedInstanceState == null) {
+
+			if (dataList.get(0).isSpinner()
+					& dataList.get(1).getTitle() != null) {
+				SelectItem(2);
+			} else if (dataList.get(0).getTitle() != null) {
+				SelectItem(1);
+			} else {
+				SelectItem(0);
+			}
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void SelectItem(int possition) {
+
+		Fragment fragment = null;
+		Bundle args = new Bundle();
+		switch (possition) {
+
+		case 2:
+			fragment = new FragmentThree();
+			args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+					.get(possition).getImgResID());
+			break;
+		case 3:
+			fragment = new FragmentOne();
+			args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 4:
+			fragment = new FragmentTwo();
+			args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 5:
+			fragment = new FragmentThree();
+			args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+					.get(possition).getImgResID());
+			break;
+		case 6:
+			fragment = new FragmentThree();
+			args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+					.get(possition).getImgResID());
+			break;
+		case 7:
+			fragment = new FragmentTwo();
+			args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 8:
+			fragment = new FragmentThree();
+			args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+					.get(possition).getImgResID());
+			break;
+		case 9:
+			fragment = new FragmentOne();
+			args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 10:
+			fragment = new FragmentTwo();
+			args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 11:
+			fragment = new FragmentThree();
+			args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList
+					.get(possition).getImgResID());
+			break;
+		case 12:
+			fragment = new FragmentOne();
+			args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 14:
+			fragment = new FragmentThree();
+			args.putString(FragmentThree.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentThree.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;	
+		case 15:
+			fragment = new FragmentOne();
+			args.putString(FragmentOne.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentOne.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		case 16:
+			fragment = new FragmentTwo();
+			args.putString(FragmentTwo.ITEM_NAME, dataList.get(possition)
+					.getItemName());
+			args.putInt(FragmentTwo.IMAGE_RESOURCE_ID, dataList.get(possition)
+					.getImgResID());
+			break;
+		default:
+			break;
+		}
+
+		fragment.setArguments(args);
+		FragmentManager frgManager = getFragmentManager();
+		frgManager.beginTransaction().replace(R.id.content_frame, fragment)
+				.commit();
+
+		mDrawerList.setItemChecked(possition, true);
+		setTitle(dataList.get(possition).getItemName());
+		mDrawerLayout.closeDrawer(mDrawerList);
+
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggles
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// The action bar home/up action should open or close the drawer.
+		// ActionBarDrawerToggle will take care of this.
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			if (dataList.get(position).getTitle() == null) {
+				SelectItem(position);
+			}
+
+		}
+	}
+
+}
